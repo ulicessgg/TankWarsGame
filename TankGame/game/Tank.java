5ee6f281b7a89a7747bbcb03064510bee5089aaa
ulices <sergioulicesgonzalezguerrero@gmail.com>
2024-07-08 22:40:44 -0700

    added collision detection for the tank class and cleaned up the angle calculation so it reverts to values between 0 and 360 for debugging the collision

--- TankGame/game/Tank.java
+++ TankGame/game/Tank.java
@@ -131,32 +131,70 @@ public class Tank{
         if (this.RightPressed) {
             this.rotateRight();
         }
+
+        System.out.println(angle); // this is for debugging the collision
+
     }
 
-    private void rotateLeft() {
+    private void rotateLeft()
+    {
         this.angle -= this.ROTATIONSPEED;
+
+        if(this.angle < 0)
+        {
+            this.angle = 360;
+        }
     }
 
-    private void rotateRight() {
+    private void rotateRight()
+    {
         this.angle += this.ROTATIONSPEED;
+
+        if(this.angle > 360)
+        {
+            this.angle = 0;
+        }
     }
 
-    private void moveBackwards() {
+    private void moveBackwards()
+    {
+        float tempX = x;
+        float tempY = y;
+
         vx =  Math.round(R * Math.cos(Math.toRadians(angle)));
         vy =  Math.round(R * Math.sin(Math.toRadians(angle)));
-        x -= vx;
-        y -= vy;
-       checkBorder();
+        if(!checkCollision(x - vx, y - vy))
+        {
+            x -= vx;
+            y -= vy;
+            checkBorder();
+        }
+        else
+        {
+            x = tempX + 1;
+            y = tempY + 1;
+        }
     }
 
-    private void moveForwards() {
+    private void moveForwards()
+    {
+        float tempX = x;
+        float tempY = y;
+
         vx = Math.round(R * Math.cos(Math.toRadians(angle)));
         vy = Math.round(R * Math.sin(Math.toRadians(angle)));
-        x += vx;
-        y += vy;
-        checkBorder();
+        if(!checkCollision(x + vx, y + vy))
+        {
+            x += vx;
+            y += vy;
+            checkBorder();
+        }
+        else
+        {
+            x = tempX - 1;
+            y = tempY - 1;
+        }
     }
-
 
     private void checkBorder()
     {
@@ -172,6 +210,24 @@ public class Tank{
         if (y >= GameConstants.GAME_SCREEN_HEIGHT - 120) {
             y = GameConstants.GAME_SCREEN_HEIGHT - 120;
         }
+    }
+
+    private boolean checkCollision(float x, float y)
+    {
+        Rectangle temp = new Rectangle((int) x, (int) y, img.getWidth(), img.getHeight());
+        for(Wall wall : wallIntel.values())
+        {
+            if(wall.isBreakable() && wall.getBounds().intersects((getBounds())))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    Rectangle getBounds()
+    {
+        return new Rectangle((int) x, (int) y, img.getWidth(), img.getHeight());
     }
 
     @Override
